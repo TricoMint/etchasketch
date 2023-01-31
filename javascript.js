f9d26184b54fdba9939916dc4e3721a9bd5ac143
TricoMint <11ldannen@gmail.com>
2023-01-30 21:33:02 -0800

    update javascript.js to activate etchasketch

--- javascript.js
+++ javascript.js
@@ -1,6 +1,17 @@
+const container = document.getElementById("container");
 
 for (i = 0; i < 256; i++) {
     const box = document.createElement("div");
-    const element = document.getElementById("container");
-    element.appendChild(box);
-};
+    box.setAttribute("class", "test");
+    box.classList.add("test2");
+    box.addEventListener("mouseover", function test() {box.classList.add("blackbox");});
+    container.appendChild(box);
+}; 
+
+
+
+/*const blackbox = document.getElementById("container");
+
+blackbox.addEventListener("mouseover", function test() {blackbox.classList.add("blackbox");});
+
+*/
